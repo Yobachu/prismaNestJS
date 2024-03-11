0292501f305eb18256243ea49571ca19eed240c4
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-03-11 16:50:22 +0300

    РеализоватьТегМодельПризма

---
+++ prisma/migrations/20240311130822_add_tags/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "tags" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "tags_pkey" PRIMARY KEY ("id")
+);
---
+++ src/tag/tag.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from "@nestjs/common";
+import {PrismaService} from "../prisma.service";
+
+@Injectable()
+export class TagService{
+    constructor(private prisma: PrismaService) {
+    }
+    async findAll(){
+        return await this.prisma.tag.findMany()
+    }
+}
