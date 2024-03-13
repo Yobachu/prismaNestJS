7165b5b1ac035358f90d66cacf7fdbac79baec02
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-03-13 22:06:20 +0300

    РеализоватьРегистрациюИЛогинПользователя

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TagModule} from "./tag/tag.module";
+import { TagModule } from './tag/tag.module';
+import { UserModule } from './tag/user/user.module';
 
 @Module({
-  imports: [TagModule],
+  imports: [TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,11 +1,10 @@
-import {Injectable} from "@nestjs/common";
-import {PrismaService} from "../prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class TagService{
-    constructor(private prisma: PrismaService) {
-    }
-    async findAll(){
-        return await this.prisma.tag.findMany()
-    }
+export class TagService {
+  constructor(private prisma: PrismaService) {}
+  async findAll() {
+    return await this.prisma.tag.findMany();
+  }
 }
